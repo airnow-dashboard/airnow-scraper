48621c482a9391aa6585a71c4796b355a93126e0
SoloSynth1 <solosynth1@gmail.com>
2022-11-24 14:31:06 -0800

    add daily.ts to fetch daily data; refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,23 @@
-type Monitor = {
-    files: object
-}
+import {HistoricalFetcher} from "./historical.js";
+import {DailyFetcher} from "./daily.js";
+import {BaseFetcher} from "./common.js";
 
-import { default as axios } from 'axios';
-import pLimit from 'p-limit';
-import * as fs from 'fs';
-import * as path from 'path';
-
-const indexUrl = "https://www.dosairnowdata.org/dos/AllPostsHistorical.json";
-const concurrencyLimit = 5;
-const outputFolder = "./output";
-
-const fetch = async () => {
-    // get the historical data from API
-    const res = await axios.get(indexUrl);
-
-    // parse the response into a list of URLs to CSVs
-    const locations: object = res.data;
-    let dataUrls = [];
-    for (const location in locations) {
-        const monitors: Monitor[] = locations[location].monitors;
-        for (const monitor of monitors) {
-            dataUrls = dataUrls.concat(Object.values(monitor.files));
-        }
+const lastArg: string = process.argv.slice(-1)[0];
+let fetcher: BaseFetcher = null;
+switch (lastArg) {
+    case 'current': {
+        fetcher = new DailyFetcher();
+        break;
     }
-
-    // set concurrency limit
-    const limit = pLimit(concurrencyLimit);
-
-    // define what to do with each URL
-    const inputs = dataUrls.map(url => limit(async () => {
-        console.log(`Downloading ${url}`);
-        const response = await axios.get(url);
-
-        const {data, request} = response;
-        const filePath = outputFolder + request.path;
-        const folderPath = path.dirname(filePath)
-
-        await fs.promises.mkdir(folderPath, { recursive: true }).catch(console.error);
-        await fs.promises.writeFile(filePath, data).catch(console.error);
-        console.log("Done.");
-    }));
-
-    // wait until all promises are returned
-    await Promise.all(inputs);
+    case 'historical': {
+        fetcher = new HistoricalFetcher();
+        break;
+    }
+    default: {
+        throw Error("unknown command");
+    }
 }
 
-fetch();
+if (fetcher) {
+    fetcher.fetch();
+}
